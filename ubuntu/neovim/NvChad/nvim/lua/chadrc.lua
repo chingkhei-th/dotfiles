b0ac0917fd5ba690594119a11dddaf470e1343bb
Chingkheinganba Thoudam <72967745+chingkhei-th@users.noreply.github.com>
2025-04-26 17:46:33 +0000

    New personal Config for NvChad

--- ubuntu/neovim/NvChad/nvim/lua/chadrc.lua
+++ ubuntu/neovim/NvChad/nvim/lua/chadrc.lua
@@ -1,12 +1,12 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 -- local M = {}
 
 -- M.base46 = {
--- 	theme = "catppuccin",
+-- 	theme = "tokyodark",
 
 -- 	-- hl_override = {
 -- 	-- 	Comment = { italic = true },
@@ -14,42 +14,49 @@
 -- 	-- },
 -- }
 
+-- -- M.nvdash = { load_on_startup = true }
+-- -- M.ui = {
+-- --       tabufline = {
+-- --          lazyload = false
+-- --      }
+-- --}
+
 -- return M
 
--- NvChad UI 3.0
+-- NvChad UI v3.0 --
 
 local options = {
 
-  -- Theme
   base46 = {
-    theme = "catppuccin", -- default theme
-    hl_add = {},
-    hl_override = {},
+    theme = "tokyodark",
+    hl_add = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
+    hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
     integrations = {},
     changed_themes = {},
-    transparency = false,
-    theme_toggle = { "catppuccin", "one_light" },
+    transparency = true,
+    theme_toggle = { "tokyonight", "tokyodark" },
   },
 
-  -- UI
   ui = {
     cmp = {
-      icons_left = false, -- only for non-atom styles!
-      lspkind_text = true,
-      style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-      transparency = true,
-      format_colors = {
-        tailwind = false, -- will work for css lsp too
-        icon = "󱓻",
-      },
+      icons_left = true, -- only for non-atom styles!
+      style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
+      abbr_maxwidth = 60,
+      -- for tailwind, css lsp etc
+      format_colors = { lsp = true, icon = "󱓻" },
     },
 
-    -- Telescope File searcher
     telescope = { style = "borderless" }, -- borderless / bordered
 
     statusline = {
       enabled = true,
-      theme = "default", -- default/vscode/vscode_colored/minimal
+      theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
       -- default/round/block/arrow separators work only for default statusline theme
       -- round and block will work for minimal theme only
       separator_style = "default",
@@ -63,35 +70,79 @@ local options = {
       lazyload = true,
       order = { "treeOffset", "buffers", "tabs", "btns" },
       modules = nil,
+      bufwidth = 21,
     },
   },
 
-  -- Dashboard
+  -- nvchad dashboard
   nvdash = {
     load_on_startup = true,
+    -- header = {
+    --   "                            ",
+    --   "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+    --   "   ▄▀███▄     ▄██ █████▀    ",
+    --   "   ██▄▀███▄   ███           ",
+    --   "   ███  ▀███▄ ███           ",
+    --   "   ███    ▀██ ███           ",
+    --   "   ███      ▀ ███           ",
+    --   "   ▀██ █████▄▀█▀▄██████▄    ",
+    --   "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+    --   "                            ",
+    --   "     Powered By  eovim    ",
+    --   "                            ",
+    -- },
     header = {
+      "⠐⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠂",
+      "⠀⢹⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀",
+      "⠀⢸⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣠⣿⡇⠀",
+      "⠀⠸⣿⣿⣷⣦⣀⡴⢶⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⣴⣾⣿⣿⠇⠀",
+      "⠀⠀⢻⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣿⣿⣿⣿⣿⡟⠀⠀",
+      "⠀⠀⣠⣻⡿⠿⢿⣫⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣻⣥⠀⠀",
+      "⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⡿⣟⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀",
+      "⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⡹⡜⠋⡾⣼⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀",
+      "⠀⠀⣿⣻⣾⣭⣝⣛⣛⣛⣛⣃⣿⣾⣇⣛⣛⣛⣛⣯⣭⣷⣿⣿⡇⠀",
+      "⠀⠰⢿⣿⣎⠙⠛⢻⣿⡿⠿⠟⣿⣿⡟⠿⠿⣿⡛⠛⠋⢹⣿⡿⢳⠀",
+      "⠀⠘⣦⡙⢿⣦⣀⠀⠀⠀⢀⣼⣿⣿⣳⣄⠀⠀⠀⢀⣠⡿⢛⣡⡏⠀",
+      "⠀⠀⠹⣟⢿⣾⣿⣿⣿⣿⣿⣧⣿⣿⣧⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀",
+      "⠀⠀⢰⣿⣣⣿⣭⢿⣿⣱⣶⣿⣿⣿⣿⣷⣶⢹⣿⣭⣻⣶⣿⣿⠀⠀",
+      "⠀⠀⠈⣿⢿⣿⣿⠏⣿⣾⣛⠿⣿⣿⣿⠟⣻⣾⡏⢿⣿⣯⡿⡏⠀⠀",
+      "⠀⠀⠤⠾⣟⣿⡁⠘⢨⣟⢻⡿⠾⠿⠾⢿⡛⣯⠘⠀⣸⣽⡛⠲⠄⠀",
+      "⠀⠀⠀⠀⠘⣿⣧⠀⠸⠃⠈⠙⠛⠛⠉⠈⠁⠹⠀⠀⣿⡟⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⢻⣿⣶⣀⣠⠀⠀⠀⠀⠀⠀⢠⡄⡄⣦⣿⠃⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠘⣿⣷⣻⣿⢷⢶⢶⢶⢆⣗⡿⣇⣷⣿⡿⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣛⣭⣭⣭⣭⣭⣻⣿⡿⠛⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠿⠟⠛⠛⠛⠻⠿⠟⠀⠀⠀⠀⠀⠀⠀⠀",
       "                            ",
-      "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
-      "   ▄▀███▄     ▄██ █████▀    ",
-      "   ██▄▀███▄   ███           ",
-      "   ███  ▀███▄ ███           ",
-      "   ███    ▀██ ███           ",
-      "   ███      ▀ ███           ",
-      "   ▀██ █████▄▀█▀▄██████▄    ",
-      "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
-      "         ◃ Ching ▹         ",
-      "     Powered By eovim     ",
+      "     𝐜𝐡𝐢𝐧𝐠  𝒗𝒊𝒎    ",
       "                            ",
     },
+    -- header = {
+    --   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣤⣶⠀⠀",
+    --   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠈⣿⣿⣇⠀",
+    --   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢾⣿⣷⣄⠀⠀⢸⣿⣿⠀",
+    --   "⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠙⢿⣿⣧⡀⠈⣿⣿⣇",
+    --   "⠀⠀⠀⠀⠀⠀⢰⣿⣿⣦⣄⡀⠀⠻⣿⣿⣦⠸⠟⠛",
+    --   "⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣷⣤⣈⠛⠁⠀⠀⠀",
+    --   "⠀⠀⠀⠀⠀⣿⣶⣶⣦⣤⣀⣙⡻⢿⠃⠀⠀⠀⠀⠀",
+    --   "⠀⠀⠀⠀⠈⠉⠙⠛⠿⢿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀",
+    --   "⢠⣤⡄⠀⣿⣿⣿⣿⣿⣷⣶⣶⣿⠀⣤⣤⡄⠀⠀⠀",
+    --   "⢸⣿⡇⠀⣉⣉⣉⣉⣉⣛⣛⣛⣟⠀⣿⣿⡇⠀⠀⠀",
+    --   "⢸⣿⡇⠀⠿⠿⠿⠿⠿⠿⠿⠿⠿⠀⣿⣿⡇⠀⠀⠀",
+    --   "⢸⣿⣧⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣿⣿⡇⠀⠀⠀",
+    --   "⠸⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠇⠀⠀⠀",
+    --   "                            ",
+    --   "     𝐜𝐡𝐢𝐧𝐠  𝒗𝒊𝒎    ",
+    --   "                            ",
+    -- },
 
     buttons = {
-      { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
-      { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
-      { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
-      { txt = "󱥚  Themes", keys = "Spc t h", cmd = ":lua require('nvchad.themes').open()" },
-      { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
+      { txt = "  Find File", keys = "f", cmd = "Telescope find_files" },
+      { txt = "  Recent Files", keys = "r", cmd = "Telescope oldfiles" },
+      { txt = "󰈭  Find Word", keys = "g", cmd = "Telescope live_grep" },
+      { txt = "󱥚  Themes", keys = "t", cmd = ":lua require('nvchad.themes').open()" },
+      { txt = "  Mappings", keys = "c", cmd = "NvCheatsheet" },
 
-      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
 
       {
         txt = function()
@@ -99,16 +150,17 @@ local options = {
           local ms = math.floor(stats.startuptime) .. " ms"
           return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
         end,
-        hl = "NvDashLazy",
+        hl = "NvDashFooter",
         no_gap = true,
       },
 
-      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
     },
   },
 
-  -- Terminal
+  -- terminal
   term = {
+    base46_colors = true,
     winopts = { number = false, relativenumber = false },
     sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
     float = {
@@ -128,7 +180,7 @@ local options = {
     excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
   },
 
-  mason = { pkgs = {} },
+  mason = { pkgs = {}, skip = {} },
 
   colorify = {
     enabled = true,
@@ -140,4 +192,3 @@ local options = {
 
 local status, chadrc = pcall(require, "chadrc")
 return vim.tbl_deep_extend("force", options, status and chadrc or {})
-

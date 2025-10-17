9d5a1de8ffd5509f910d6ccb7cd785f5c908f981
Chingkheinganba Thoudam <72967745+chingkhei-th@users.noreply.github.com>
2025-10-17 15:48:24 +0530

    Refractor and resetup new nvim configuration

--- windows/neovim/NvChad/nvim-backup/lua/treesitter.lua
+++
@@ -1,5 +0,0 @@
-require("nvim-treesitter.configs").setup({
-    ensure_installed = { "python", "c", "html", "css", "javascript", "lua", "markdown", "markdown_inline" },
-    highlight = { enable = true },
-    indent = { enable = true }
-})
